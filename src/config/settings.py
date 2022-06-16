833b63e9d7fea0d4279d93e6fae96ce059c013e2
dexiang <52363496+little-evan-yin@users.noreply.github.com>
2022-06-16 17:09:04 +0800

    modify some setting params

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import environ
 from distutils.util import strtobool
 from pathlib import Path
 
@@ -21,19 +22,26 @@ django_stubs_ext.monkeypatch()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=True)
+DOT_ENV_FILE = env("DJANGO_DOT_ENV_FILE", default=".env")
+if READ_DOT_ENV_FILE:
+    print("=== start to setting safe-config-service env ===")
+    print(str(BASE_DIR.parent / DOT_ENV_FILE))
+    env.read_env(str(BASE_DIR.parent / DOT_ENV_FILE))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", None)
+SECRET_KEY = env("SECRET_KEY", None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(strtobool(os.getenv("DEBUG", "false")))
+DEBUG = env.bool("DEBUG", False)
 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#std:setting-ALLOWED_HOSTS
-allowed_hosts = os.getenv("DJANGO_ALLOWED_HOSTS", ".localhost,127.0.0.1,[::1]")
-ALLOWED_HOSTS = [allowed_host.strip() for allowed_host in allowed_hosts.split(",")]
 
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["*"])
 # Application definition
 
 REST_FRAMEWORK = {
@@ -101,7 +109,7 @@ LOGGING = {
     },
     "root": {
         "handlers": ["console"],
-        "level": os.getenv("ROOT_LOG_LEVEL", "INFO"),
+        "level": env("ROOT_LOG_LEVEL", default="INFO"),
     },
     "loggers": {
         "LoggingMiddleware": {
@@ -113,7 +121,7 @@ LOGGING = {
 }
 
 ROOT_URLCONF = "config.urls"
-FORCE_SCRIPT_NAME = os.getenv("FORCE_SCRIPT_NAME", default=None)
+FORCE_SCRIPT_NAME = env("FORCE_SCRIPT_NAME", default=None)
 
 TEMPLATES = [
     {
@@ -139,11 +147,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_NAME", "postgres"),
-        "USER": os.getenv("POSTGRES_USER", "postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
-        "HOST": os.getenv("POSTGRES_HOST", "db"),
-        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        "NAME": env("POSTGRES_NAME", default="postgres"),
+        "USER": env("POSTGRES_USER", default="postgres"),
+        "PASSWORD": env("POSTGRES_PASSWORD", default="postgres"),
+        "HOST": env("POSTGRES_HOST", default="db"),
+        "PORT": env("POSTGRES_PORT", default="5432"),
     }
 }
 
@@ -194,8 +202,8 @@ SWAGGER_SETTINGS = {
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_URLS_REGEX = r"^/api/.*$"
 
-CGW_URL = os.environ.get("CGW_URL")
-CGW_FLUSH_TOKEN = os.environ.get("CGW_FLUSH_TOKEN")
+CGW_URL = env("CGW_URL", default="")
+CGW_FLUSH_TOKEN = env("CGW_FLUSH_TOKEN", default="")
 
 # By default, Django stores files locally, using the MEDIA_ROOT and MEDIA_URL settings.
 # (using the default the default FileSystemStorage)
@@ -204,22 +212,22 @@ MEDIA_ROOT = f"{BASE_DIR}/media/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
 MEDIA_URL = "/media/"
 
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_CUSTOM_DOMAIN = os.getenv("AWS_S3_CUSTOM_DOMAIN")
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID", default=None)
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY", default=None)
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", default=None)
+AWS_S3_CUSTOM_DOMAIN = env("AWS_S3_CUSTOM_DOMAIN", default=None)
 # By default files with the same name will overwrite each other. Set this to False to have extra characters appended.
 AWS_S3_FILE_OVERWRITE = True
 # Setting AWS_QUERYSTRING_AUTH to False to remove query parameter authentication from generated URLs.
 # This can be useful if your S3 buckets are public.
 AWS_QUERYSTRING_AUTH = False
-DEFAULT_FILE_STORAGE = os.getenv(
-    "DEFAULT_FILE_STORAGE", "storages.backends.s3boto3.S3Boto3Storage"
+DEFAULT_FILE_STORAGE = env(
+    "DEFAULT_FILE_STORAGE", default="storages.backends.s3boto3.S3Boto3Storage"
 )
 
 # SECURITY
 # https://docs.djangoproject.com/en/4.0/ref/settings/#csrf-trusted-origins
-allowed_csrf_origins = os.getenv("CSRF_TRUSTED_ORIGINS", "")
+allowed_csrf_origins = env("CSRF_TRUSTED_ORIGINS", default="")
 if allowed_csrf_origins:
     CSRF_TRUSTED_ORIGINS = [
         allowed_csrf_origins.strip()
